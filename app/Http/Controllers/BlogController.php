c5e44c116cc0b3db9c521496b4787a908d9150f2
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-09-17 13:02:20 +0900

    記事投稿
    what
    記事投稿機能の実装
    why
    根幹機能のひとつであるため

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Blog;
+use App\Http\Requests\BlogRequest;
 
 class BlogController extends Controller
 {
@@ -36,5 +37,36 @@ class BlogController extends Controller
 
         return view('blog.detail',['blog' => $blog]);
 
+    }
+
+    /**
+     * ブログ登録画面を表示
+     * @return view
+     */
+    public function showCreate(){
+        return view('blog.form');
+    }
+    
+    /**
+     * ブログ登録
+     * 
+     */
+    public function exeStore(BlogRequest $request)
+    {
+        //ブログのデータを受け取る
+        $inputs = $request->all();
+
+        \DB::beginTransaction();
+        try{
+            //ブログを登録
+            Blog::create($inputs);
+            \DB::commit();
+        }catch(\Throwable $e){
+            \DB::rollback();
+            abort(500);
+        }
+        
+        \Session::flash('err_msg', 'ブログを登録しました');
+        return redirect(route('blogs'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@
 //ブログ一覧を表示
 Route::get('/', 'BlogController@showList')->name('blogs');
 
+//ブログ登録画面を表示
+Route::get('/blog/create', 'BlogController@showCreate')->name('create');
+
+//ブログ登録
+Route::post('/blog/store', 'BlogController@exeStore')->name('store');
+
 //ブログ詳細を表示
 Route::get('/blog/{id}', 'BlogController@showDetail')->name('show');
